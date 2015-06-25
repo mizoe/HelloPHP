3854c51df9ecf83bfb6aa3237fd84bbcaaba2460
Tomo Mizoe <mz@july.co.jp>
2015-06-25 13:57:59 +0900

    rensyu5

---
+++ rensyu5.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>無題ドキュメント</title>
+</head>
+
+<body>
+<?php
+
+define("TSUBO", 3.3);
+printf("101号室→%.2f坪<BR>", 100/TSUBO);
+printf("102号室→%.2f坪<BR>",  75/TSUBO);
+printf("103号室→%.2f坪",     150/TSUBO);
+
+?>
+
+
+</body>
+</html>
