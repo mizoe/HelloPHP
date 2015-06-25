821ac0b2625c11a8a237cd7d98066084db2fd8ca
Tomo Mizoe <mz@july.co.jp>
2015-06-25 11:10:12 +0900

    練習問題1

---
+++ rensyu1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>無題ドキュメント</title>
+</head>
+
+<body>
+
+<?php
+$familyName = "溝江";
+$firstName = "智徳";
+?>
+
+<table border="1">
+  <tr>
+    <td>姓</td>
+    <td>名</td>
+  </tr>
+  <tr>
+    <td><?php echo $familyName; ?></td>
+    <td><?php echo $firstName; ?></td>
+  </tr>
+</table>
+
+
+</body>
+</html>
