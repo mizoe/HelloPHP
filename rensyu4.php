e46f489b1d1d3d37ca25c7748b60117a963804fa
Tomo Mizoe <mz@july.co.jp>
2015-06-25 13:53:37 +0900

    rensyu4

---
+++ rensyu4.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>無題ドキュメント</title>
+</head>
+
+<body>
+<?php
+
+$msg = "セ・リーグは２４日、都内で臨時理事会を開き、東日本大震災の影響で、当初の２５日から２９日に延期していた開幕日を再度延期し、パ・リーグと同じ４月１２日に変更することを決めた。";
+$msg25 = mb_substr($msg, 0, 25);
+print $msg25 . '…<a href="http://www.nikkansports.com/baseball/news/f-bb-tp0-20110324-752531.html">続きはこちら</a>';
+
+?>
+
+
+</body>
+</html>
