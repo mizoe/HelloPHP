e0520a79e48389b1431358cabaecd35487682a6a
Tomo Mizoe <mz@july.co.jp>
2015-07-08 15:03:22 +0900

    Chap6 complete

---
+++ c6/EvenOdd.php
@@ -0,0 +1,11 @@
+<?php
+class EvenOdd{
+	public function JudgeEvenOdd($num){
+		if($num % 2 == 1){
+			return "奇数です";
+		}else{
+			return "偶数です";
+		}
+	}
+}
+?>
---
+++ c6/index4.php
@@ -0,0 +1,29 @@
+<?php
+require_once('EvenOdd.php');
+$res = "";
+if(isset($_POST['submit'])){
+	$evenOdd = new EvenOdd();
+	$res = $evenOdd->JudgeEvenOdd($_POST['num']);
+}
+?>
+
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>無題ドキュメント</title>
+</head>
+
+<body>
+<?php print_r($_POST); ?>
+
+<h1>初めてのクラス</h1>
+<form method="post" action="">
+<label>数字を入力<input type="text" name="num" required></label>
+<input type="submit" value="判定" name="submit">
+</form>
+
+<?php echo $res; ?>
+</body>
+</html>
